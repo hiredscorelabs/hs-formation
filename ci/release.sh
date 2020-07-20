3639361cedc1ea855a9fcc4357ea488e2d3a95cc
Avner Cohen <israbirding@gmail.com>
2020-07-20 16:55:22 +0300

    [CI] bump version from 4.1.6 to 4.1.7

--- ci/release.sh
+++ ci/release.sh
@@ -1,4 +1,4 @@
-set +x 
+set -x 
 ## Bump version using poetry and tag version, assumes minor version patch
 
 BUMP_ACTION=${1:-patch} 
@@ -10,5 +10,5 @@ echo "__version__ = '$NEW_VER'" > hs_formation/__version__.py
 sed "s/version = \"${OLD_VER}\"/version = \"${NEW_VER}\"/g" pyproject.toml | tee pyproject.toml
 git commit -am"[CI] bump version from ${OLD_VER} to ${NEW_VER}"
 git tag "v${NEW_VER}"
-git push $TARGET_REMOTE && git push --tags
+git push $TARGET_REMOTE --tags
 poetry run publish --build
