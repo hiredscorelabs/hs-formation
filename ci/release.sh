0ed0e36773ecb1a452f678efd0142e6ab6e5c190
Avner Cohen <israbirding@gmail.com>
2020-07-20 16:42:26 +0300

    CI syntax error and end-to-end testing

--- ci/release.sh
+++ ci/release.sh
@@ -1,3 +1,4 @@
+set +x 
 ## Bump version using poetry and tag version, assumes minor version patch
 
 BUMP_ACTION=${1:-patch} 
@@ -6,8 +7,8 @@ OLD_VER=$(poetry version | cut -d' ' -f 2)
 poetry version $BUMP_ACTION
 NEW_VER=$(poetry version | cut -d' ' -f 2)
 echo "__version__ = '$NEW_VER'" > hs_formation/__version__.py
-sed "s/version = \"${OLD_VER}\"/version = \"${NEW_VER}\"/g" pyproject.toml > pyproject.toml
+sed "s/version = \"${OLD_VER}\"/version = \"${NEW_VER}\"/g" pyproject.toml | tee pyproject.toml
 git commit -am"[CI] bump version from ${OLD_VER} to ${NEW_VER}"
-git tag "v${NEW_VER}"
-git push $TARGET_REMOTE && git push --tags
-run poetry publish --build
+##git tag "v${NEW_VER}"
+##git push $TARGET_REMOTE && git push --tags
+##poetry run publish --build
